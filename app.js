34311b1657f657a743484cb5a74df2d478713de9
Akiva <calculatinghchance2@live.com>
2018-12-18 14:55:40 -0500

    restored master to orginal checkpoint prescribed form

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-<<<<<<< HEAD
 
-=======
-  var   removeList = document.getElementById('delete');
->>>>>>> checkpoint-5-DOM-Assignment
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
@@ -22,17 +18,11 @@ function onReady() {
     //set the input's type to checkbox
     checkbox.type = "checkbox";
 
-    let minusBtn = document.createElement('button');
-  //  let minusBtn.createElement('button');
-    minusBtn.innerHTML = "<span>Delete</span>";
-
     //set the title
     newLi.textContent = title;
 
     //attach the checkbox to the Li
     newLi.appendChild(checkbox);
-
-    newLi.appendChild(minusBtn);
 
     //attach the li to the ul
     toDoList.appendChild(newLi);
@@ -40,42 +30,8 @@ function onReady() {
     //empty the input
     newToDoText.value = '';
 
-
-     minusBtn.addEventListener('click', function() {
-       newLi.parentNode.removeChild(newLi)
-     });
-
   });
 
-//   let minusBtn.createElement('button');
-//   minusBtn.innerHTML = <span>Delete</span>;
-//   minusBtn.addEventListener('click', () => {
-//
-// });
-
-//   removeList.addEventListener('minusBtn', event => {
-//     event.preventDefault();
-//
-//     let minusBtn.createElement('button');
-//     minusBtn.innerHTML =
-//     minusBtn.addEventListener('click', () => {
-//
-// });
-//
-//     let temp.innerHTML
-//     //get text
-//     let title = newRemoveText.value;
-//
-//     //create new input
-//     let checkbox = document.createElement('input');
-//
-//     //set input's type to checkbox
-//      checkbox.type = 'checkbox';
-//
-//
-//     //to remove from Checklist
-//     let throwawayNode = toDoList.removeChild('title');
-//   });
 }
 window.onload = function() {
   onReady();
